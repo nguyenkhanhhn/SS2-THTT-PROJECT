8c24c4b7324d780e974cbbdd0eacb2f925471430
nguyenkhanhhn <52152972+nguyenkhanhhn@users.noreply.github.com>
2019-06-26 15:48:01 +0700

    Add bai tap thuc thanh toan tu

---
+++ Bai1-java.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputHeight;
+inputwidth = prompt(' chieu rộng là: ');
+inputHeight = prompt( 'chiều dài là :');
+let width = parseInt(inputwidth);
+let Height = parseInt(inputHeight);
+let are = width * Height;
+document.write("Diện tích là :"  + are);
